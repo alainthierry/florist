3e74634bcec7a0c2e10a9f57695f6f017be05a57
alainthierry <thierryal98@gmail.com>
2021-04-02 18:25:53 +0100

    admin interface configuration

--- store/admin.py
+++ store/admin.py
@@ -4,6 +4,24 @@ from store.models import (
 )
 """ Register models for admin """
 
-admin.site.register(Customer)
-admin.site.register(Booking)
-admin.site.register(Flower)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+	list_filter = ['name', 'email']
+	list_display = ('name', 'email', 'phone_number')
+
+@admin.register(Flower)
+class FlowerAdmin(admin.ModelAdmin):
+    list_filter = ['created_at',]
+    list_display = ('name', 'description', 'price', 'available', 'image_url')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_filter = ['delivered',]
+    list_display = ('customer', 'flower', 'booked_date', 'delivered')
+ 
+
+
+
+
