be6d8b7bb4c816a20ba0c0b0d6f1f8c9f7399e12
alainthierry <thierryal98@gmail.com>
2021-04-02 18:24:35 +0100

    urls configuration

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,7 @@ app_name = 'store'
 
 urlpatterns = [
 	re_path(r'^search/', views.search, name='search'),
-	
+	re_path(r'^flower/(?P<flower_id>[0-9]+)/$', views.detail, name='detail'),
+	re_path(r'^$', views.listing, name='listing'),
+	re_path(r'^about/', views.about, name='about'),
 ]
